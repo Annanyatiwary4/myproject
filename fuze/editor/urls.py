31410c2dec6de3c54041b56aa542d8952caef331
Annanya Tiwary <annanyatiwary4@gmail.com>
2024-10-03 00:35:11 +0530

    tmhe kya mai kch v chng kru

--- fuze/editor/urls.py
+++ fuze/editor/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.CreateRoomView, name='home'),  # Use CreateRoomView for home if it handles room creation
-    path('create-room/', views.CreateRoomView, name='create_room'),
+    path('room/', views.CreateRoomView, name='room'),
     path('join-room/', views.JoinRoomView, name='join_room'),
     path('room/<str:room_id>/<str:username>/', views.RoomView, name='room'),
 ]
